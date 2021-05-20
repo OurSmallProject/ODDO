83091c7099a6b0e9b492ae6acc5ad46b1809a612
Kirill Chernov <kirillchernov@gmail.com>
2021-05-21 02:43:18 +0300

    make it better

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY,
+    mongoURI: "mmongodb+srv://admin:admin123@oddo.7nrrx.mongodb.net/ODDOdb?retryWrites=true&w=majority",
+    secretOrKey: "supersecret",
     geocoderProvider: process.env.GEOCODER_PROVIDER,
     geocoderAPIKey: process.env.GEOCODER_API_Key
 };
