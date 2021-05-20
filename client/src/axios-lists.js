8985b4ebcc4c328efa759653ecd5cf99b0a97f7d
Kirill Chernov <kirillchernov@gmail.com>
2021-05-20 19:58:14 +0300

    edit main logic in client

---
+++ client/src/axios-lists.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+let serverURL = 'http://localhost:8081';
+
+//TODO use hostname here
+if(process.env.NODE_ENV === 'production'){
+    serverURL = 'https://NAME.herokuapp.com/';
+}
+
+const instance = axios.create({
+   baseURL: serverURL
+});
+
+export default instance;
